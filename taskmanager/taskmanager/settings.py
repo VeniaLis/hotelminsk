d707d31bed3eea7f52a2af1ae464fd6cc6fb3283
Venialis <nfs41738@mail.ru>
2022-03-30 23:10:58 +0300

    python

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -47,6 +47,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
